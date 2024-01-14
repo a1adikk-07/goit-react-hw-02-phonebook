23f4e3421bc8046d2e23d2c0b4937db16874f798
a1adikk-07 <136816822+a1adikk-07@users.noreply.github.com>
2024-01-14 16:42:51 +0200

    commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,20 @@
+import styles from '../ContactList/contact-list.module.css';
+
+const ContactList = ({ items, deleteContact }) => {
+  const elements = items.map(({ id, name, number }) => (
+    <li key={id} className={styles.contacts}>
+      {name}: {number}{' '}
+      <button onClick={() => deleteContact(id)} type="button">
+        Delete
+      </button>
+    </li>
+  ));
+
+  return (
+    <>
+      <ul className={styles.list}>{elements}</ul>
+    </>
+  );
+};
+
+export default ContactList;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,17 @@
+import style from '../Filter/filter.module.css';
+
+const Filter = ({ changeFilter }) => {
+  return (
+    <div className={style.filter}>
+      <label htmlFor="filterID">Find contact by name</label>
+      <input
+        onChange={changeFilter}
+        id="filterID"
+        name="filter"
+        placeholder="Search"
+      />
+    </div>
+  );
+};
+
+export default Filter;
